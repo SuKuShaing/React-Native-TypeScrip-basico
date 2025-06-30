1722579e108832f9393b208dcf8be1c13c28883c
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-06-30 16:32:02 -0400

    AuthContext, eliminando erasableSyntaxOnly tsconfig.app.json

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,12 +1,30 @@
 import { useAuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
-    const { hola } = useAuthContext();
-    
-	return (
-		<>
-			<h3>LoginPage</h3>
-            <p>{ hola }</p>
-		</>
-	);
+    const { isChecking, isAuthenticated, isNotAuthenticated } = useAuthContext();
+
+    if (isChecking) {
+        return (
+            <>
+                <h3>LoginPage</h3>
+                <p>Checking...</p>
+            </>
+        );
+    }
+
+    if (isAuthenticated) {
+        return (
+            <>
+                <h3>LoginPage</h3>
+                <p>Authenticated</p>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <h3>LoginPage</h3>
+            <p>Not Authenticated</p>
+        </>
+    );
 };
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,31 @@
-import { createContext, useContext, type PropsWithChildren } from "react";
+import { createContext, useContext, useEffect, useState, type PropsWithChildren } from "react";
+
+// export type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+// export const AuthStatusEnum = {
+//     Checking: 'checking' as AuthStatus,
+//     Authenticated: 'authenticated' as AuthStatus,
+//     NotAuthenticated: 'not-authenticated' as AuthStatus,
+// };
+
+enum AuthStatus {
+	Checking = 'checking', // sí no se coloca nada, se coloca el valor por defecto que es la posición del enum, en este caso Checking = 0
+	Authenticated = 'authenticated', // en este caso Authenticated = 1
+	NotAuthenticated = 'not-authenticated', // en este caso NotAuthenticated = 2
+}
 
 interface AuthState {
-	hola: string;
+	status: AuthStatus;
+	token?: string;
+	user?: User;
+	isChecking: boolean;
+	isAuthenticated: boolean;
+	isNotAuthenticated: boolean;
+}
+
+interface User {
+	name: string;
+	email: string;
 }
 
 export const AuthContext = createContext({} as AuthState);
@@ -9,10 +33,24 @@ export const AuthContext = createContext({} as AuthState);
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
+
+	const [status, setStatus] = useState(AuthStatus.Checking);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setStatus(AuthStatus.NotAuthenticated);
+		}, 1500);
+	}, []);
+	
 	return (
 		<AuthContext.Provider
 			value={{
-				hola: "mundo",
+				status: status,
+
+				// Getter
+				isChecking: status === AuthStatus.Checking,
+				isAuthenticated: status === AuthStatus.Authenticated,
+				isNotAuthenticated: status === AuthStatus.NotAuthenticated,
 			}}
 		>
 			{children}
